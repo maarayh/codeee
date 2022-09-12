7ae0e450f7329d8d12dd77258663a3f0a13d318c
Ammara Zafar <41151522+maarayh@users.noreply.github.com>
2022-09-12 18:27:13 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Demo from './Components/Demo';
+import Person from './Components/Person';
+
+
+function App() {
+  return (
+  <>
+   <Demo name='Ammara' age='25'/>
+   <Person tutorial="React"/>
+   <Person tutorial="SEO"/>
+   </>
+  );
+}
+
+export default App;
